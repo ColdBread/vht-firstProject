bd5ce526992f04d7aa123a3bb5e9208c358121b1
Dimka0777 <sdimka0777@gmail.com>
2017-02-25 14:30:33 +0200

    first commit

---
+++ src/main/java/com/google/training/helloworld/Multiplication.java
@@ -0,0 +1,11 @@
+package com.google.training.helloworld;
+
+public class Multiplication {
+	public int result = 0;
+		public Multiplication(int f, int s){
+			result = f*s;
+		}
+		public int getMessage(){
+			return result;
+		}
+}
